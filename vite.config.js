98b0a5f35105704a03c363e24f9ffbac00a1c4d0
Byakuya2709 <khanhb2110127@student.ctu.edu.vn>
2024-12-07 21:56:06 +0700

    final-1

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/B2110127_TaskManagement_FrontEnd/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
